5e4bdab988ad2d5b60f8fc17e28cd30c8cb1aa30
SegunCodes <shegstix64@gmail.com>
2023-09-09 19:01:53 +0200

    install entity core framework tools and sql server

---
+++ Data/BloggieDbContext.cs
@@ -0,0 +1,15 @@
+using bloggie.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace bloggie.Data 
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
